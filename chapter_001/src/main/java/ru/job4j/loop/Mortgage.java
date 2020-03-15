ce2157c47cc278b5447ec65484c3d0487ebaabdf
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-03-15 20:35:48 +0700

    5.7. Ипотека.[#235753] UPD

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 1;
         while (salary < amount * (1 + percent / 100)) {
-            amount = (int) (amount * percent / 100) - salary;
+            amount =  amount * (int) (1 + percent / 100) - salary;
             year++;
         }
         return year;
--- chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -19,4 +19,11 @@ public class MortgageTest {
         int year = mortgage.year(100, 120, 50);
         assertThat(year, is(2));
     }
+
+    @Test
+    public void when5Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(500, 120, 10);
+        assertThat(year, is(5));
+    }
 }
