12b039c9f21abeb760df60bd3d2f9f2e5130d447
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-03-18 09:30:09 +0700

    6.5. Поиск индекса в диапазоне.[#235689] UPD2

--- chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -2,18 +2,12 @@ package ru.job4j.array;
 
 public class BarleyBreak {
     public static void main(String[] args) {
+        int c = 1;
         int[][] table = new int[3][3];
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table[i].length; j++) {
-                table[i][j] = i * table[i].length + j + 1;
+                table[i][j] = c++;
             }
-        }
-
-        for (int[] ints : table) {
-            for (int anInt : ints) {
-                System.out.print(anInt);
-            }
-            System.out.println();
         }
     }
 }
