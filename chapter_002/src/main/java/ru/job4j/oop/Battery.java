c5658e674d8a6edccf60fb3844f1632b030dfe4c
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2020-04-07 23:58:28 +0700

    1.7. Состояние объекта.[#235856]

--- chapter_002/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -3,6 +3,9 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
+    public Battery(int initial) {
+        load = initial;
+    }
     public void exchange(Battery another) {
         another.load += this.load;
         this.load = 0;
