bd7b87b61305fe8b37083894957957cb51ed325a
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2020-04-15 12:41:33 +0700

    some codestyle editions

--- chapter_002/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_002/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,15 +7,15 @@ public class PointTest {
 
     @Test
     public void distance() {
-        Point a = new Point(0,0);
-        Point b = new Point(100,0);
+        Point a = new Point(0, 0);
+        Point b = new Point(100, 0);
         Assert.assertEquals(100, a.distance(b), 0.01);
     }
 
     @Test
     public void distance3d() {
-        Point a = new Point(0,0, 0);
-        Point b = new Point(100,0, 0);
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(100, 0, 0);
         Assert.assertEquals(100, a.distance(b), 0.01);
     }
 }
--- chapter_002/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_002/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 public class TriangleTest {
     @Test
     public void areaOfTwoSidesOnAxes() {
-        Triangle t = new Triangle(new Point(0, 1), new Point(1, 0), new Point(0,0));
+        Triangle t = new Triangle(new Point(0, 1), new Point(1, 0), new Point(0, 0));
         Assert.assertEquals(0.5, t.area(), 0.01);
     }
 
     @Test
     public void areaOfOneSideOnAxes() {
-        Triangle t = new Triangle(new Point(0, 10), new Point(10, 10), new Point(0,0));
+        Triangle t = new Triangle(new Point(0, 10), new Point(10, 10), new Point(0, 0));
         Assert.assertEquals(50, t.area(), 0.01);
     }
 }
