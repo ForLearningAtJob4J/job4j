18f0d621c8304c7f5b97818a084b979d695c7d76
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2020-04-19 01:36:05 +0700

    1.1. Тесты на ValidateInput.[#235842]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        String[] data = {"one", "1"};
+        ValidateStubInput input = new ValidateStubInput(data);
+        input.askInt("Enter");
+        assertThat(
+                out.toString(),
+                is(String.format("Please enter valid data again.%n"))
+        );
+    }
+
+    @Test
+    public void whenInvalidInputMaxNumber() {
+        String[] data = {"8", "1"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("Enter", 6);
+        assertThat(
+                out.toString(),
+                is(String.format("Please select key from menu.%n"))
+        );
+    }
+
+    @Test
+    public void whenInvalidInputMinNumber() {
+        String[] data = {"-100", "1"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("Enter", 6);
+        assertThat(
+                out.toString(),
+                is(String.format("Please select key from menu.%n"))
+        );
+    }
+}
