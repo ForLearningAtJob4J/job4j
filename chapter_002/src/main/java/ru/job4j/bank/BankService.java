0f37ca3b58b018b9d509028fcd7a91d7807da673
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2020-05-07 19:10:35 +0700

    3. Банковские переводы.[#235794] UPD

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -44,7 +44,13 @@ public class BankService {
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        if (Objects.isNull(srcAccount)) {
+            return false;
+        }
         Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (Objects.isNull(destAccount)) {
+            return false;
+        }
         if (srcAccount.getBalance() - amount >= 0) {
             srcAccount.setBalance(srcAccount.getBalance() - amount);
             destAccount.setBalance(destAccount.getBalance() + amount);
