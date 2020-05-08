f548ee0bd56afcacd95a0a2b4c8562d4c63ac6d6
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2020-05-08 19:18:05 +0700

    0. Сортировка [#235782] UPD

--- chapter_002/src/test/java/ru/job4j/tracker/ItemByNameDescComparatorTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ItemByNameDescComparatorTest.java
@@ -3,14 +3,13 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class ItemByNameComparatorTest {
+public class ItemByNameDescComparatorTest {
 
     @Test
     public void compare() {
@@ -18,18 +17,7 @@ public class ItemByNameComparatorTest {
                 new Item("Second"),
                 new Item("First"),
                 new Item("Third")));
-        list.sort(new ItemByNameComparator());
-        assertThat(new String[]{list.get(0).getName(), list.get(1).getName(), list.get(2).getName()},
-                is(new String[]{"First", "Second", "Third"}));
-    }
-
-    @Test
-    public void compareReversed() {
-        ArrayList<Item> list = new ArrayList<>(List.of(
-                new Item("Second"),
-                new Item("First"),
-                new Item("Third")));
-        list.sort(new ItemByNameComparator().reversed());
+        list.sort(new ItemByNameAscComparator().reversed());
         assertThat(new String[]{list.get(0).getName(), list.get(1).getName(), list.get(2).getName()},
                 is(new String[]{"Third", "Second", "First"}));
     }
