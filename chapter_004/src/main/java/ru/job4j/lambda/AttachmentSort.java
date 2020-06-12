5a2e8e12264170d41afcbf23ac256f019e0b3d0a
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-06-12 17:37:33 +0700

    3. Ссылки на методы. [#235640]

--- chapter_004/src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ chapter_004/src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -8,24 +8,17 @@ public class AttachmentSort {
     public static void main(String[] args) {
         List<Attachment> attachments = Arrays.asList(
                 new Attachment("image 1", 100),
-                new Attachment("image 2", 34),
-                new Attachment("image 3", 13)
+                new Attachment("image 22", 34),
+                new Attachment("image 333", 13)
         );
-        Comparator<Attachment> comparator =  new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getSize() - o2.getSize();
-            }
-        };
+        Comparator<Attachment> comparator = Comparator.comparingInt(Attachment::getSize);
         attachments.sort(comparator);
         System.out.println(attachments);
-        comparator =  new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        };
+        comparator = Comparator.comparing(Attachment::getName);
         attachments.sort(comparator);
+        System.out.println(attachments);
+
+        attachments.sort((left, right) -> right.getName().length() - left.getName().length());
         System.out.println(attachments);
     }
 }
